85a6f94df5e5c18bace0926fc4329c07b9648326
Becky McClain <beckykelley8@yahoo.com>
2018-03-03 15:41:48 -0700

    trying something different

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,113 +1,69 @@
 $(document).ready(function() {
 
 
-    function startGame() {
-        $("#t").hide();
-        $("#question").hide();
-        $("#a").html("start");
-        $("#b").hide();
-        $("#c").hide();
-        $("#d").hide();
-      }
 
+  var currentQuestion = [
+    {
+      question: "What kind of pet does Harry take to school?",
+      options: ["Frog", "Cat", "Owl", "Snake"],
+      correct: 3
+    },
+    {
+      question: "What position does Harry play on the quidditch team?",
+      options: ["Seeker", "Chaser", "Beater", "Bludger"],
+      correct: 1
+    },
+    {
+      question: "Which of the following was not a pet of Hagrid's",
+      options: ["Norbert", "Chester", "Fang", "Buckbeak"],
+      correct: 2
+    }
+  ];
+
+  function startGame() {
+    $("#t").hide();
+    $("#question").hide();
+    $("#a").html("start");
+    $("#b").hide();
+    $("#c").hide();
+    $("#d").hide();
+  }
+
+  $("#a").on("click", function() {
+    //write code to begin timer
+    //for loop to display question and buttons that correlate
+    $("#t").show();
+    // countdown();
+    $("#question").show();
+    $("#question").replaceWith(currentQuestion[0].question);
+    $("#b").show();
+    $("#c").show();
+    $("#d").show();
+    $("#a").html(currentQuestion[0].options[0]);
+    $("#b").html(currentQuestion[0].options[1]);
+    $("#c").html(currentQuestion[0].options[2]);
+    $("#d").html(currentQuestion[0].options[3]);
 
     $("#a").on("click", function() {
-        //write code to begin timer
-        //for loop to display question and buttons that correlate
-        for (var i = 0; i < 1; i++) {
-          $("#question").html(questArray[0].questionText);
-          $("#b").show();
-          $("#c").show();
-          $("#d").show();
-          $("#a").html(questArray[0].ansArray[0].answerA);
-          $("#b").html(questArray[0].ansArray[0].answerB);
-          $("#c").html(questArray[0].ansArray[0].answerC);
-          $("#d").html(questArray[0].ansArray[0].answerD);
-        }
+      
+    }
     
-        $("#a").on("click", function() {
-          if ("#a" === correctAnswer[i]) {
-            console.log("yes!");
-            correct();
-          } else {
-            console.log("no!");
-            wrong();
-          }
-        });
-
-        var questArray = [question1, question2, question3]
-
-  var questions = [
-      {
-    question1Text: "What kind of pet does Harry take to school?",
-    ans1Array: [
-      {
-        answerA: "Frog",
-        isRight: false
-      },
-      {
-        answerB: "Cat",
-        isRight: false
-      },
-      {
-        answerC: "Owl",
-        isRight: true
-      },
-      {
-        answerD: "Snake",
-        isRight: false
-      }
-    ],
-    imgSrc: "assets/images/Hedwig.jpg"
-      },
-
-{
-    question2Text: "What position does Harry play on the quidditch team?",
-    ans2Array: [
-      {
-        answerA: "Seeker",
-        isRight: true
-      },
-      {
-        answerB: "Chaser",
-        isRight: false
-      },
-      {
-        answerC: "Beater",
-        isRight: false
-      },
-      {
-        answerD: "Bludger",
-        isRight: false
-      }
-    ],
-    imgSrc: "assets/images/Hedwig.jpg"
-  },
-{
-      question3Text: "Which of the following was not a pet of Hagrid's",
-    ans3Array: [
-      {
-        answerA: "Norbert",
-        isRight: false
-      },
-      {
-        answerB: "Chester",
-        isRight: true
-      },
-      {
-        answerC: "Fang",
-        isRight: false
-      },
-      {
-        answerD: "Buckbeak",
-        isRight: false
-      }
-    ],
-    imgSrc: "assets/images/Hedwig.jpg"
-  }]
-
-    });
-
-    startGame();
 });
 
+startGame();
+var timeLeft = 10;
+var elem = document.getElementById("timer");
+
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+    ranOutOfTime();
+  } else {
+    elem.innerHTML = timeLeft + " seconds remaining";
+    timeLeft--;
+  }
+}
+
+})
